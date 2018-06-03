cd4daf473d17da09b94c45243d9858198e3f8863
truelav <madan.serghei@yahoo.com>
2018-06-02 17:43:45 -0700

    adding player init

--- public/src/bin/player.js
+++ public/src/bin/player.js
@@ -1,13 +1,14 @@
-function Player(piece, name) {
-    this.name = name;
+function Player(piece, name, color) {
     this.cell = 0;
+    this.name = name;
     this.piece = piece;
-    this.money = money;
+    this.color = color;
+    this.money = 1500;
     this.inJail = false;
     this.property = [];
     this.getOutOfJail = 0;
-    turn = 0;
-
+    this.turn = 0;
+    this.position = 0;
 }
 
 module.exports = {
